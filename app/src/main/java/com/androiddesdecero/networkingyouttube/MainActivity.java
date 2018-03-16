c9febb34526ea5275cbbc559a44d58935e6aa238
Alberto <info@androiddesdecero.com>
2018-03-16 16:33:01 +0100

    Fin

--- app/src/main/java/com/androiddesdecero/networkingyouttube/MainActivity.java
+++ app/src/main/java/com/androiddesdecero/networkingyouttube/MainActivity.java
@@ -1,10 +1,14 @@
 package com.androiddesdecero.networkingyouttube;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,15 +26,27 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private static final int TIEMPO_LOADER_ID = 1;
     private static final String REQUEST_URL = "http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1";
     private TextView textView;
-    private TextView textViewTemperatura;
+    private TextView textViewNoInternet;
     private TiempoAdapter tiempoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LoaderManager loaderManager = getLoaderManager();
-        loaderManager.initLoader(TIEMPO_LOADER_ID, null, this);
+
+        textViewNoInternet = findViewById(R.id.textViewNoInternet);
+        textViewNoInternet.setVisibility(View.GONE);
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(TIEMPO_LOADER_ID, null, this);
+        } else {
+            textViewNoInternet.setVisibility(View.VISIBLE);
+        }
 
         ListView tiempoListView = (ListView) findViewById(R.id.list);
         tiempoAdapter = new TiempoAdapter(this, new ArrayList<Tiempo>());
